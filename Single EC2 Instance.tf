742b25af7f78ee1b6a1c466c9f0499a459428b03
andrew.fanning@gmail.com <andrew.fanning@gmail.com>
2020-05-20 16:07:58 -0500

    Putting in init-script like functionality.

--- Single EC2 Instance.tf
+++ Single EC2 Instance.tf
@@ -14,7 +14,24 @@ resource "aws_instance" "example" {
   ami           = "ami-0323c3dd2da7fb37d"
   instance_type = "t2.micro"
 
+connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/SiriusAWSKeys.pem")
+    host        = self.public_ip
+}
+
 provisioner "local-exec" {
     command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
   }
+
+provisioner "remote-exec" {
+    inline = [
+        "sudo yum update -y",
+        "sudo yum install httpd -y",
+        "service httpd start",
+        "chkconfig httpd on",
+        "echo "<html><h1>Welcome To My Webpage on Web01</h1></html>" > index.html",
+  ]
+}
 }
